cf59cc97ce9cebe1bb4d2330616168d4529622f2
Alestor Aldous <alestoraldous@gmail.com>
2020-07-02 06:59:09 +0530

    added first function for replacing x and other special char

--- calc.py
+++ calc.py
@@ -1,2 +1,8 @@
 from tkinter import *
 import math 
+class calc: 
+  
+    def getandreplace(self): 
+        self.expression = self.e.get() 
+        self.newtext=self.expression.replace('/','/') 
+        self.newtext=self.newtext.replace('x','*') 

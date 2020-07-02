5e0ae3725188b59f7cb6743fa39df34b37ef66c3
Alestor Aldous <alestoraldous@gmail.com>
2020-07-02 07:05:10 +0530

    added equal and other functions

--- calc.py
+++ calc.py
@@ -6,3 +6,50 @@ class calc:
         self.expression = self.e.get() 
         self.newtext=self.expression.replace('/','/') 
         self.newtext=self.newtext.replace('x','*') 
+    def squareroot(self): 
+        self.getandreplace() 
+        try: 
+        
+            self.value= eval(self.newtext)  
+        except SyntaxError or NameError: 
+            self.e.delete(0,END) 
+            self.e.insert(0,'Invalid Input!') 
+        else: 
+            self.sqrtval=math.sqrt(self.value) 
+            self.e.delete(0,END) 
+            self.e.insert(0,self.sqrtval) 
+  
+    def square(self): 
+        self.getandreplace() 
+        try: 
+            self.value= eval(self.newtext)  
+        except SyntaxError or NameError: 
+            self.e.delete(0,END) 
+            self.e.insert(0,'Invalid Input!') 
+        else: 
+            self.sqval=math.pow(self.value,2) 
+            self.e.delete(0,END) 
+            self.e.insert(0,self.sqval) 
+
+    def equals(self): 
+        self.getandreplace() 
+        try: 
+            self.value= eval(self.newtext)  
+        except SyntaxError or NameError: 
+            self.e.delete(0,END) 
+            self.e.insert(0,'Invalid Input!') 
+        else: 
+            self.e.delete(0,END) 
+            self.e.insert(0,self.value) 
+     def clearall(self): 
+            self.e.delete(0,END) 
+  
+    def clear1(self): 
+            self.txt=self.e.get()[:-1] 
+            self.e.delete(0,END) 
+            self.e.insert(0,self.txt) 
+  
+    def action(self,argi): 
+            self.e.insert(END,argi) 
+  
+ 
